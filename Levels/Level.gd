245b0272cf9c786d2565496e760f12d8dfc2415f
Alexander Ignatov <yalishanda@abv.bg>
2020-10-11 15:05:46 +0300

    Create empty project from template

---
+++ Levels/Level.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+export (String, FILE, "*.tscn") var Next_Scene: String
+
+func _ready()->void:
+	Hud.visible = true
+	PauseMenu.can_show = true
+
+func _on_Button_pressed()->void:
+	Game.emit_signal("ChangeScene", Next_Scene)
+
+func _exit_tree()->void:
+	Hud.visible = false
+	PauseMenu.can_show = false
